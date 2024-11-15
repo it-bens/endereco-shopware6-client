51fd315d66bc5ad995ec5c92e7dd340f6135c0a5
Martin Bens <martin.bens@it-bens.de>
2024-11-15 20:32:43 +0100

    A console command was added to update the custom fields of all orders or a by-id defined list of orders. This allows plugin users to update existing orders that were created before this changes were introduced.

---
+++ src/Console/UpdateOrdersCustomFieldsCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Endereco\Shopware6Client\Console;
+
+use Endereco\Shopware6Client\Service\OrdersCustomFieldsUpdaterInterface;
+use Shopware\Core\Framework\Adapter\Console\ShopwareStyle;
+use Shopware\Core\Framework\Context;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+#[AsCommand(UpdateOrdersCustomFieldsCommand::COMMAND_NAME, UpdateOrdersCustomFieldsCommand::COMMAND_DESCRIPTION)]
+class UpdateOrdersCustomFieldsCommand extends Command
+{
+    public const COMMAND_NAME = 'endereco:address-validation:order-custom-fields-update';
+    public const COMMAND_DESCRIPTION =
+        'Update the custom fields of orders based on the persisted order address validation data.';
+
+    protected static string $defaultName = self::COMMAND_NAME;
+    protected static string $defaultDescription = self::COMMAND_DESCRIPTION;
+
+    private Context $context;
+    private OrdersCustomFieldsUpdaterInterface $ordersCustomFieldsUpdater;
+
+    public function __construct(
+        OrdersCustomFieldsUpdaterInterface $ordersCustomFieldsUpdater
+    ) {
+        // The method is internal since Shopware 6.6.1.0. After that, the `createCliContext` method can be used.
+        $this->context = Context::createDefaultContext();
+        $this->ordersCustomFieldsUpdater = $ordersCustomFieldsUpdater;
+
+        parent::__construct();
+    }
+
+    public function configure(): void
+    {
+        $this->addArgument(
+            'order-ids',
+            InputArgument::IS_ARRAY,
+            'IDs of the orders for which the custom fields should be updated',
+        );
+        $this->setHelp('Providing no order IDs will lead to an update of all orders.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new ShopwareStyle($input, $output);
+
+        $orderIds = $input->getArgument('order-ids') ?? [];
+        if (!is_array($orderIds)) {
+            throw new \InvalidArgumentException('The Order ID list must be an array');
+        }
+        $orderIds = array_filter($orderIds);
+        $orderIds = array_unique($orderIds);
+
+        if (count($orderIds) === 0) {
+            $io->info('No order IDs were provided. All orders will be updated.');
+        }
+        if (count($orderIds) > 0) {
+            $io->info('The orders with IDs: ' . implode(', ', $orderIds) . ' will be updated.');
+        }
+
+        $this->ordersCustomFieldsUpdater->updateOrdersCustomFields($orderIds, [], $this->context);
+
+        return Command::SUCCESS;
+    }
+}
