a1b8407e9e4a5e15639e9adeeb0af97c7de4c3d2
Martin Bens <martin.bens@it-bens.de>
2024-11-16 16:57:08 +0100

    A subscriber for address extensions `entity.written` events was added to update the `amsRequestPayload` field if the data in the extension changes. The logic contains early returns and is only executed if the `amsTimestamp` changed to prevent looping/recursive updates. The `amsTimestamp` only changes if a new validation was performed.

--- src/Service/AddressCheck/AddressCheckPayloadBuilderInterface.php
+++ src/Service/AddressCheck/AddressCheckPayloadBuilderInterface.php
@@ -3,7 +3,9 @@
 namespace Endereco\Shopware6Client\Service\AddressCheck;
 
 use Endereco\Shopware6Client\Model\AddressCheckPayload;
+use Endereco\Shopware6Client\Model\AddressCheckData;
 use Shopware\Core\Checkout\Customer\Aggregate\CustomerAddress\CustomerAddressEntity;
+use Shopware\Core\Checkout\Order\Aggregate\OrderAddress\OrderAddressEntity;
 use Shopware\Core\Framework\Context;
 
 interface AddressCheckPayloadBuilderInterface
@@ -20,4 +22,15 @@ interface AddressCheckPayloadBuilderInterface
         CustomerAddressEntity $addressEntity,
         Context $context
     ): AddressCheckPayload;
+
+    /**
+     * @param CustomerAddressEntity|OrderAddressEntity $addressEntity
+     * @param Context $context
+     *
+     * @return AddressCheckData
+     */
+    public function buildAddressCheckPayloadWithoutLanguage(
+        $addressEntity,
+        Context $context
+    ): AddressCheckData;
 }
